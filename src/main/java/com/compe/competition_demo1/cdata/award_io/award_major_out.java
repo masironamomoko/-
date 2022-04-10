ff00698d4d54cdde78236120135b17cb8b2affda
masironamomoko <1044732874@qq.com>
2022-04-10 17:01:43 +0800

    num5

--- src/main/java/com/compe/competition_demo1/cdata/award_io/award_major_out.java
+++ src/main/java/com/compe/competition_demo1/cdata/award_io/award_major_out.java
@@ -1,4 +1,21 @@
 package com.compe.competition_demo1.cdata.award_io;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data   //添加getter/setter
+@NoArgsConstructor     //添加无参构造器
+@AllArgsConstructor     //添加全参构造器
+@Accessors(chain = true)    //添加链式调用
 public class award_major_out {
+    private Integer allc;
+    private Integer allp;
+    @Data
+    public class major{
+        private Integer major1;
+        private Integer major2;
+        private Integer major3;
+    }
 }
--- src/main/java/com/compe/competition_demo1/cdata/competitionsth/searchpass/searchpassCom.java
+++ src/main/java/com/compe/competition_demo1/cdata/competitionsth/searchpass/searchpassCom.java
@@ -1,5 +1,6 @@
 package com.compe.competition_demo1.cdata.competitionsth.searchpass;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import java.util.Date;
 
 public class searchpassCom {
     private Integer com_id;
-    @JSONField(pattern="yyyy-MM-dd")
+    @JSONField(format="yyyy-MM-dd")
     private Date com_date;
     private String com_mainname;
     private Integer com_manager;
